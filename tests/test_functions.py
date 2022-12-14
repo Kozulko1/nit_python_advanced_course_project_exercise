5467df47df0d4dff1779f7d3c559277ef2ac2f97
Ivan Kožul <kozul.ivan97@gmail.com>
2022-12-14 22:55:08 +0100

    Add test for print contacts

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -9,5 +9,15 @@ class TestFunctions(unittest.TestCase):
     def setUp(self) -> None:
         self.logged_user = User("Valentino", "123")
         self.other_user = User("Budlak", "123")
+        self.logged_user.contacts = self.other_user
         self.user_storage = UserStorage()
         self.user_storage.log_in(self.logged_user)
+
+    @patch("user_manager.scripts.functions.print")
+    @patch("user_manager.scripts.functions.logging.error")
+    def test__print_contacts(self, mock_logerror: MagicMock, mock_print: MagicMock):
+        print_contacts(self.logged_user, self.user_storage)
+        mock_print.assert_called_once_with("Username: Budlak")
+
+        print_contacts(self.other_user, self.user_storage)
+        mock_logerror.assert_called_once_with("Budlak is not logged in.")
