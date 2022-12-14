08ffe52dd82a2cacc6fba0b217dc13f55442cb10
Valentin Radicevic <valentin.radicevic@tttech-auto.com>
2022-12-14 20:17:35 +0100

    Save

---
+++ user_manager/scripts/user_bank.py
@@ -0,0 +1,26 @@
+from user import User
+class Users:
+    def __init__(self):
+        self.registered: list[User] = list()
+        self.logged_in: list[User] = list()
+
+    def add_to_registered(self, user: User):
+        if user not in self.registered:
+            self.registered.append(user)
+        else:
+            print("Već je registrovan")
+
+    def remove_from_registered(self, user:User):
+        if user in self.registered:
+            self.registered.remove(user)
+        else:
+            print("Već je neregistrovan")
+
+    def log_in(self, user:User):
+        if user not in self.logged_in:
+            self.logged_in.append(user)
+        else:
+            print("Već je logiran")
+
+    def is_logged_in(self, user:User):
+        pass
